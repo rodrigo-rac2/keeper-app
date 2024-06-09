df8b1b98d1a64e3a358743ea2cec0e3d98faa08f
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2024-06-09 12:28:44 -0300

    add: carousel (#6)
    * add: carousel
    
    * add: carousel tests

--- src/components/MainPage/FeatureSection/FeatureSection.jsx
+++ src/components/MainPage/FeatureSection/FeatureSection.jsx
@@ -1,5 +1,7 @@
 // src/components/FeatureSection.jsx
 import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./FeatureSection.css";
 
 function FeatureSection({ title, description, images }) {
@@ -7,15 +9,24 @@ function FeatureSection({ title, description, images }) {
     <div className="feature-section">
       <h2>{title}</h2>
       <p>{description}</p>
-      <div className="images-container">
-        {images.map((imageSrc, index) => (
-          <img
-            key={index}
-            src={imageSrc}
-            alt={`Feature ${index}`}
-            className="feature-image"
-          />
-        ))}
+      <div className="carousel-container">
+        <Carousel 
+          showArrows={true} 
+          autoPlay={true} 
+          infiniteLoop={true} 
+          showThumbs={false} // Hides the thumbnails
+          className="custom-carousel"
+        >
+          {images.map((imageSrc, index) => (
+            <div key={index}>
+              <img
+                src={imageSrc}
+                alt={`Feature ${index}`}
+                className="feature-image"
+              />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
